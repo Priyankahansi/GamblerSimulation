c7ddc9613734041e748a080eac27500c8fcda457
Priyanka <priyankahansi95@gmail.com>
2022-04-26 13:54:07 +0530

    added a welcome message

---
+++ src/com/bridgelabz/GamblerSimulation.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class GamblerSimulation {
+
+    static void method1() {
+        System.out.println("welocme to Gambler simulation");
+    }
+    public static void main(String[] args) {
+        method1();
+    }
+
+}
